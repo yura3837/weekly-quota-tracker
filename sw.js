2d274f482ea19ad2d1a0fbaf5d321d1f4553c3b7
yura3837 <yura3837@users.noreply.github.com>
2026-06-02 22:28:27 +0900

    Initial weekly tracker app

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "weekly-quota-tracker-v1";
+const ASSETS = [
+  "./weekly-tracker.html",
+  "./manifest.webmanifest",
+  "./weekly-tracker-icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
